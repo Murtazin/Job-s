f59931f55825c94076011244c8168c4388d60746
Murtazin <renatmurtazin@yahoo.com>
2022-10-17 14:16:32 +0300

    Initial Commit

---
+++ Jobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jobs
+//
+//  Created by Renat Murtazin on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
