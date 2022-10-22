a643782d3f898a0d7dfa74f1773f5f5c9056bdaa
Murtazin <renatmurtazin@yahoo.com>
2022-10-22 15:49:07 +0300

    Layouted VacancyTableViewCell.

---
+++ Jobs/Source/Common/UIView/NavigationWordView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationWordView.swift
+//  Jobs
+//
+//  Created by Renat Murtazin on 22.10.2022.
+//
+
+import UIKit
+
+class NavigationWordView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Jobs/Source/UI/Screens/Search/Main/VacancyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VacancyTableViewCell.swift
+//  Jobs
+//
+//  Created by Renat Murtazin on 22.10.2022.
+//
+
+import UIKit
+
+class VacancyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
